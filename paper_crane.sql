02d909e0f0b688038589813f1c4bfb9f7bc2000f
Debora Kwon <95396914+Debora-k@users.noreply.github.com>
2023-01-20 08:43:29 -0700

    add database file

---
+++ paper_crane.sql
@@ -0,0 +1,283 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.1
+-- Dumped by pg_dump version 15.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: client; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.client (
+    user_id integer NOT NULL,
+    client_name character varying(50) NOT NULL,
+    website character varying(300)
+);
+
+
+ALTER TABLE public.client OWNER TO postgres;
+
+--
+-- Name: employee; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employee (
+    user_id integer NOT NULL,
+    employee_name character varying(50) NOT NULL,
+    employee_role character(1) NOT NULL,
+    CONSTRAINT employee_employee_role CHECK ((employee_role = ANY (ARRAY['C'::bpchar, 'D'::bpchar, 'A'::bpchar])))
+);
+
+
+ALTER TABLE public.employee OWNER TO postgres;
+
+--
+-- Name: employee_project_bridge; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employee_project_bridge (
+    user_id integer NOT NULL,
+    project_lead_id integer NOT NULL,
+    project_id integer NOT NULL
+);
+
+
+ALTER TABLE public.employee_project_bridge OWNER TO postgres;
+
+--
+-- Name: project; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.project (
+    project_id integer NOT NULL,
+    client_id integer NOT NULL,
+    project_lead_id integer NOT NULL
+);
+
+
+ALTER TABLE public.project OWNER TO postgres;
+
+--
+-- Name: task; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.task (
+    project_id integer NOT NULL,
+    task_id integer NOT NULL,
+    description character varying(500) NOT NULL,
+    deadline date NOT NULL,
+    start_date date NOT NULL,
+    expected_work_hours double precision NOT NULL,
+    progress_in_work_hours double precision DEFAULT 0
+);
+
+
+ALTER TABLE public.task OWNER TO postgres;
+
+--
+-- Name: time_off; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.time_off (
+    time_off_id integer NOT NULL,
+    employee_id integer NOT NULL,
+    start_date date,
+    end_date date,
+    status character(1) NOT NULL,
+    reason character varying(500) NOT NULL
+);
+
+
+ALTER TABLE public.time_off OWNER TO postgres;
+
+--
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."user" (
+    user_id integer NOT NULL,
+    type character varying(20) NOT NULL,
+    email character varying(100) NOT NULL,
+    password character varying(100) NOT NULL,
+    CONSTRAINT user_email CHECK (((email)::text ~~ '%_@__%.__%'::text))
+);
+
+
+ALTER TABLE public."user" OWNER TO postgres;
+
+--
+-- Name: video_training; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.video_training (
+    video_id integer NOT NULL,
+    video_link character varying(300) NOT NULL
+);
+
+
+ALTER TABLE public.video_training OWNER TO postgres;
+
+--
+-- Data for Name: client; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.client (user_id, client_name, website) FROM stdin;
+\.
+
+
+--
+-- Data for Name: employee; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.employee (user_id, employee_name, employee_role) FROM stdin;
+\.
+
+
+--
+-- Data for Name: employee_project_bridge; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.employee_project_bridge (user_id, project_lead_id, project_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: project; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.project (project_id, client_id, project_lead_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: task; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.task (project_id, task_id, description, deadline, start_date, expected_work_hours, progress_in_work_hours) FROM stdin;
+\.
+
+
+--
+-- Data for Name: time_off; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.time_off (time_off_id, employee_id, start_date, end_date, status, reason) FROM stdin;
+\.
+
+
+--
+-- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."user" (user_id, type, email, password) FROM stdin;
+\.
+
+
+--
+-- Data for Name: video_training; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.video_training (video_id, video_link) FROM stdin;
+\.
+
+
+--
+-- Name: client client_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.client
+    ADD CONSTRAINT client_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: employee employee_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employee
+    ADD CONSTRAINT employee_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: employee_project_bridge employee_project_bridge_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employee_project_bridge
+    ADD CONSTRAINT employee_project_bridge_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: project project_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.project
+    ADD CONSTRAINT project_pkey PRIMARY KEY (project_id);
+
+
+--
+-- Name: time_off status; Type: CHECK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.time_off
+    ADD CONSTRAINT status CHECK ((status = ANY (ARRAY['A'::bpchar, 'R'::bpchar, 'P'::bpchar]))) NOT VALID;
+
+
+--
+-- Name: task task_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.task
+    ADD CONSTRAINT task_pkey PRIMARY KEY (project_id, task_id);
+
+
+--
+-- Name: time_off time_off_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.time_off
+    ADD CONSTRAINT time_off_pkey PRIMARY KEY (time_off_id);
+
+
+--
+-- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: video_training video_training_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.video_training
+    ADD CONSTRAINT video_training_pkey PRIMARY KEY (video_id);
+
+
+--
+-- Name: time_off employee_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.time_off
+    ADD CONSTRAINT employee_id FOREIGN KEY (employee_id) REFERENCES public.employee(user_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
