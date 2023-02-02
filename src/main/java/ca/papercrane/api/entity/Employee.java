221878e3b5a426f14ecd89abd44ed04503afa802
rchesh <reececheshire@outlook.com>
2023-02-02 00:32:47 -0700

    feat: Edited Employee class.
    Follows the inheritance for the user, client classes. References the database for what we have in the employee table at the moment.

--- src/main/java/ca/papercrane/api/entity/Employee.java
+++ src/main/java/ca/papercrane/api/entity/Employee.java
@@ -1,53 +1,58 @@
-package ca.papercrane.api.employee;
+package ca.papercrane.api.entity;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
-import java.util.Scanner;
+/**
+ * Represents an employee in our database. An Employee may do the following:
+ * <p>
+ * 1) Access video training courses
+ * 2) Request time off
+ * 3) Log their working time per project through a form
+ * 4) View past hours submissions in a calendar layout
+ * 5) Add and modify client accounts
+ */
+@Entity
+@Table(name = "employee")
+public final class Employee extends Client {
 
-@EntityScan
-public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int userId;
-    private String name;
-    private char role;
-    /**
-     * The method uses a Scanner object to read input from the user
-     * and create a new Employee object with the given name and role.
-     * The employeeService.saveEmployee(emp) line would save the new employee object to the database.
-     */
-    private EmployeeService employeeService;
+    @Column(name = "employee_name", nullable = false)
+    private String employeeName;
+
+    @Column(name = "employee_role", nullable = false)
+    private String employeeRole;
 
     public Employee() {
-        // TODO Auto-generated constructor stub
-        this.userId = userId;
-        this.name = name;
-        this.role = role;
+        // default no-args constructor.
     }
 
-    //getters and setters
-    public int getUserId() {
-        return userId;
+    /**
+     * Constructs a new Employee object with the given employee name and role.
+     *
+     * @param type         TODO: Find out what the type represents.
+     * @param email        The email tied to this Employee user account.
+     * @param password     The password tied to this Employee user account.
+     * @param clientName   The client name of the Client
+     * @param website      The website of the Client
+     * @param employeeName The employee name of the Employee
+     * @param employeeRole The employee role of the Employee
+     */
+    public Employee(String type, String email, String password, String clientName, String website,
+                    String employeeName, String employeeRole) {
+        super(type, email, password, clientName, website);
+        this.employeeName = employeeName;
+        this.employeeRole = employeeRole;
     }
 
-    public void setUserId(int userId) {
-        this.id = id;
-    }
-
+    //Get the name of the employee.
     public String getName() {
-        return name;
+        return employeeName;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public char getRole() {
-        return role;
-    }
-
-    public void setRole(char role) {
-        this.role = role;
+    //Set the new role for this employee.
+    public void setRole(String newEmployeeRole) {
+        this.employeeRole = newEmployeeRole;
     }
 
     private void register_emp_account() {
