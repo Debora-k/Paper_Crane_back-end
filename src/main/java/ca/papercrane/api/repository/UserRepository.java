3a15bd9890918c15f4e298e2c6cae0648f15201b
Hashem Al-wadeai <hashem321975@gmail.com>
2023-02-14 16:57:06 -0700

    Edit Repositories and Services

---
+++ src/main/java/ca/papercrane/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ca.papercrane.api.repository;
+
+import ca.papercrane.api.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findById(Integer userId);
+    List<User> findByType(String type);
+  }
