5f3785de4ca86134d8d4ba83054544ab23f6aa0f
Hashem Al-wadeai <hashem321975@gmail.com>
2023-02-14 16:56:39 -0700

    Edit Repository and Services Edit EmployeeRepository, UserRepository, ClientRepository and add @Transactional to all Services

--- src/main/java/ca/papercrane/api/service/TimeOffRequestService.java
+++ src/main/java/ca/papercrane/api/service/TimeOffRequestService.java
@@ -4,6 +4,7 @@ import ca.papercrane.api.repository.TimeOffRequestRepository;
 import ca.papercrane.api.request.TimeOffRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
  * A Service class for retrieving and altering data in the {@link TimeOffRequestRepository}
  */
 @Service
+@Transactional
 public record TimeOffRequestService(TimeOffRequestRepository timeOffRepository) {
 
     @Autowired
