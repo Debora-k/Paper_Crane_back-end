a0421576f972498b85920d1d5f934de93d42772c
rchesh <reececheshire@outlook.com>
2023-03-07 13:38:44 -0700

    feat: update project service.

--- src/main/java/ca/papercrane/api/service/ProjectService.java
+++ src/main/java/ca/papercrane/api/service/ProjectService.java
@@ -2,11 +2,25 @@ package ca.papercrane.api.service;
 
 import ca.papercrane.api.project.Project;
 
+import java.util.List;
+
 public interface ProjectService {
 
     Project findById(Integer projectId);
 
+    Integer createProject(Integer clientId, Integer projectLeadId, String description);
+
+    Integer createProject(Project project);
+
+    void updateProject(Project project);
+
     void save(Project project);
+
+    void delete(Project project);
+
+    void deleteById(Integer projectId);
+
+    List<Project> findByClientId(Integer clientId);
 
     Long totalCount();
 
--- src/main/java/ca/papercrane/api/service/impl/ProjectServiceImpl.java
+++ src/main/java/ca/papercrane/api/service/impl/ProjectServiceImpl.java
@@ -7,6 +7,8 @@ import ca.papercrane.api.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public final class ProjectServiceImpl implements ProjectService {
 
@@ -19,8 +21,45 @@ public final class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
+    public Integer createProject(Integer clientId, Integer projectLeadId, String description) {
+        final Project createdProject = projectRepository.save(new Project(clientId, projectLeadId, description));
+        return createdProject.getProjectId();
+    }
+
+    @Override
+    public Integer createProject(Project project) {
+        final Project createdProject = projectRepository.save(project);
+        return createdProject.getProjectId();
+    }
+
+    @Override
+    public void updateProject(Project project) {
+        final Project existingProject = findById(project.getProjectId());
+        existingProject.setClientId(project.getClientId());
+        existingProject.setProjectLeadId(project.getProjectLeadId());
+        existingProject.setProjectDescription(project.getProjectDescription());
+        projectRepository.save(existingProject);
+    }
+
+    @Override
     public void save(Project project) {
         projectRepository.save(project);
+    }
+
+    @Override
+    public void delete(Project project) {
+        projectRepository.delete(project);
+    }
+
+    @Override
+    public void deleteById(Integer projectId) {
+        final Project project = findById(projectId);
+        projectRepository.delete(project);
+    }
+
+    @Override
+    public List<Project> findByClientId(Integer clientId) {
+        return projectRepository.findAllById(clientId).orElseThrow(() -> new ResourceNotFoundException("Projects not found with clientId"));
     }
 
     @Override
