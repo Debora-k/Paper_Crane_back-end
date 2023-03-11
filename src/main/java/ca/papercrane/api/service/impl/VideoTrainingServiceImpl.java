38e045c7ed966935b6589f0650b931d4e687db42
rchesh <reececheshire@outlook.com>
2023-03-11 13:12:45 -0700

    fix: re-implement taskservice

--- src/main/java/ca/papercrane/api/service/impl/VideoTrainingServiceImpl.java
+++ src/main/java/ca/papercrane/api/service/impl/VideoTrainingServiceImpl.java
@@ -14,13 +14,44 @@ public final class VideoTrainingServiceImpl implements VideoTrainingService {
     private VideoTrainingRepository trainingRepository;
 
     @Override
-    public VideoTraining findById(Integer videoId) {
-        return trainingRepository.findById(videoId).orElseThrow(() -> new ResourceNotFoundException("Training not found!"));
+    public VideoTraining getByVideoId(Integer videoId) {
+        return trainingRepository.findByVideoId(videoId).orElseThrow(() -> new ResourceNotFoundException("Training not found for id: " + videoId));
+    }
+
+    @Override
+    public Integer create(Integer projectId, String videoLink, String description) {
+        final VideoTraining createdTraining = trainingRepository.save(new VideoTraining(projectId, videoLink, description));
+        return createdTraining.getVideoId();
+    }
+
+    @Override
+    public Integer create(VideoTraining training) {
+        final VideoTraining createdTraining = trainingRepository.save(training);
+        return createdTraining.getVideoId();
+    }
+
+    @Override
+    public void update(VideoTraining training) {
+        final VideoTraining existingTraining = getByVideoId(training.getVideoId());
+        existingTraining.setDescription(training.getDescription());
+        existingTraining.setVideoLink(training.getVideoLink());
+        save(existingTraining);
     }
 
     @Override
     public void save(VideoTraining training) {
         trainingRepository.save(training);
+    }
+
+    @Override
+    public void delete(VideoTraining training) {
+        trainingRepository.delete(training);
+    }
+
+    @Override
+    public void deleteByVideoId(Integer videoId) {
+        final VideoTraining training = getByVideoId(videoId);
+        trainingRepository.delete(training);
     }
 
     @Override
