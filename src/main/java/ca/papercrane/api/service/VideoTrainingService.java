5e480eafbcc5b3a5ea9c7fb9fc05574a421f96df
rchesh <reececheshire@outlook.com>
2023-03-11 13:11:53 -0700

    chore: re-implement videotrainingservice

--- src/main/java/ca/papercrane/api/service/VideoTrainingService.java
+++ src/main/java/ca/papercrane/api/service/VideoTrainingService.java
@@ -4,9 +4,19 @@ import ca.papercrane.api.project.training.VideoTraining;
 
 public interface VideoTrainingService {
 
-    VideoTraining findById(Integer videoId);
+    VideoTraining getByVideoId(Integer videoId);
+
+    Integer create(Integer projectId, String videoLink, String description);
+
+    Integer create(VideoTraining training);
+
+    void update(VideoTraining training);
 
     void save(VideoTraining training);
+
+    void delete(VideoTraining training);
+
+    void deleteByVideoId(Integer videoId);
 
     Long totalCount();
 
