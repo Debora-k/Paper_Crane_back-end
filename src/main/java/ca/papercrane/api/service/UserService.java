f3ed9004b41574c78949d17ac91ccc6f2d4e8ff3
rchesh <reececheshire@outlook.com>
2023-03-11 13:12:11 -0700

    fix: re-implement userservice

--- src/main/java/ca/papercrane/api/service/UserService.java
+++ src/main/java/ca/papercrane/api/service/UserService.java
@@ -4,9 +4,21 @@ import ca.papercrane.api.entity.User;
 
 public interface UserService {
 
-    User findById(Integer userId);
+    User getByUserId(Integer userId);
+
+    User getByEmail(String email);
+
+    Integer create(String type, String email, String password);
+
+    Integer create(User user);
+
+    void update(User user);
 
     void save(User user);
+
+    void delete(User user);
+
+    void deleteByUserId(Integer userId);
 
     Long totalCount();
 
