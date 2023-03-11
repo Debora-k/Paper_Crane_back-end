137cb8045965d559d4c243c4627baf35981957a0
rchesh <reececheshire@outlook.com>
2023-03-11 13:11:06 -0700

    feat: rename and add more methods to TaskRepository

--- src/main/java/ca/papercrane/api/repository/TaskRepository.java
+++ src/main/java/ca/papercrane/api/repository/TaskRepository.java
@@ -4,11 +4,14 @@ import ca.papercrane.api.project.task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
-    Optional<Task> findById(Integer taskId);
+    Optional<Task> findByTaskId(Integer taskId);
+
+    Optional<List<Task>> findAllByProjectId(Integer projectId);
 
 }
