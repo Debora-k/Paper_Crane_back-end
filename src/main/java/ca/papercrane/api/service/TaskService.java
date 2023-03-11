05cbab14c1a9071b7e077ba59cb70dd5810a2411
rchesh <reececheshire@outlook.com>
2023-03-11 13:12:22 -0700

    fix: re-implement taskservice

--- src/main/java/ca/papercrane/api/service/TaskService.java
+++ src/main/java/ca/papercrane/api/service/TaskService.java
@@ -2,11 +2,26 @@ package ca.papercrane.api.service;
 
 import ca.papercrane.api.project.task.Task;
 
+import java.util.Date;
+import java.util.List;
+
 public interface TaskService {
 
-    Task findById(Integer taskId);
+    Task getByTaskId(Integer taskId);
+
+    List<Task> getAllByProjectId(Integer projectId);
+
+    Integer create(Integer projectId, String description, Date startDate, Date deadline, Double expectedWorkHours);
+
+    Integer create(Task task);
+
+    void update(Task task);
 
     void save(Task task);
+
+    void delete(Task task);
+
+    void deleteByTaskId(Integer taskId);
 
     Long totalCount();
 
