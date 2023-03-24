3ad5d28deb9d8af1590b3ed90e67a7aae54f221f
Hashem Al-wadeai <hashem321975@gmail.com>
2023-03-24 15:06:26 -0600

    Ticket
    Added Ticket table, Class, Repo, Service

--- src/main/java/ca/papercrane/api/repository/TicketRepository.java
+++ src/main/java/ca/papercrane/api/repository/TicketRepository.java
@@ -1,13 +1,18 @@
 package ca.papercrane.api.repository;
 
+<<<<<<< Updated upstream
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+=======
+import ca.papercrane.api.project.task.Task;
+>>>>>>> Stashed changes
 import ca.papercrane.api.project.ticket.Ticket;
 
 import java.util.List;
 import java.util.Optional;
 
+<<<<<<< Updated upstream
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
@@ -16,3 +21,18 @@ public interface TicketRepository extends JpaRepository<Ticket, Integer> {
     List<Ticket> findByStatus(char status);
 
 }
+=======
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+	Optional<Task> findByTicketId(Integer ticketId);
+	
+	List<Ticket> findAllByProjectId(Integer projectId);
+
+    Optional<List<Ticket>> findByTitleAndProjectId(String title, Integer projectId);
+}
+
+>>>>>>> Stashed changes
