8969bf49f24247b3c53b1ccd14f52ea9d40d12fc
Hashem Al-wadeai <hashem321975@gmail.com>
2023-03-30 04:32:50 -0600

    fixed TicketRepo

--- src/main/java/ca/papercrane/api/repository/TicketRepository.java
+++ src/main/java/ca/papercrane/api/repository/TicketRepository.java
@@ -1,38 +1,23 @@
 package ca.papercrane.api.repository;
 
-<<<<<<< Updated upstream
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-=======
+
 import ca.papercrane.api.project.task.Task;
->>>>>>> Stashed changes
+
 import ca.papercrane.api.project.ticket.Ticket;
 
 import java.util.List;
 import java.util.Optional;
 
-<<<<<<< Updated upstream
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
     Optional <Ticket> findByTicketId(Integer ticketId);
     List<Ticket> findAllByProjectId(Integer projectId);
     List<Ticket> findByStatus(char status);
-
-}
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-
-	Optional<Task> findByTicketId(Integer ticketId);
-	
-	List<Ticket> findAllByProjectId(Integer projectId);
-
     Optional<List<Ticket>> findByTitleAndProjectId(String title, Integer projectId);
 }
 
->>>>>>> Stashed changes
