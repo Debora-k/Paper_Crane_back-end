6300481d80e0e59b023725e041c46ad2c52154b4
rchesh <20080666+rchesh@users.noreply.github.com>
2023-04-06 00:34:24 -0600

    chore: add lombok annotations to project class

--- src/main/java/ca/papercrane/api/project/Project.java
+++ src/main/java/ca/papercrane/api/project/Project.java
@@ -1,7 +1,13 @@
 package ca.papercrane.api.project;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "project")
 public final class Project {
@@ -17,45 +23,12 @@ public final class Project {
     @Column(name = "project_lead_id", nullable = false)
     private Integer projectLeadId;
 
-    //TODO: Add to database if we want to use a description.
     @Column(name = "project_description")
     private String projectDescription;
-
-    public Project() {
-
-    }
 
     public Project(Integer clientId, Integer projectLeadId, String projectDescription) {
         this.clientId = clientId;
         this.projectLeadId = projectLeadId;
-        this.projectDescription = projectDescription;
-    }
-
-    public Integer getProjectId() {
-        return projectId;
-    }
-
-    public Integer getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(Integer clientId) {
-        this.clientId = clientId;
-    }
-
-    public Integer getProjectLeadId() {
-        return projectLeadId;
-    }
-
-    public void setProjectLeadId(Integer projectLeadId) {
-        this.projectLeadId = projectLeadId;
-    }
-
-    public String getProjectDescription() {
-        return projectDescription;
-    }
-
-    public void setProjectDescription(String projectDescription) {
         this.projectDescription = projectDescription;
     }
 
