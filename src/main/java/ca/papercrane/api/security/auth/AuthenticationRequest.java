b96df20b8465ad91a52351987adf2f5ee9571100
rchesh <20080666+rchesh@users.noreply.github.com>
2023-04-06 00:29:50 -0600

    feat: add authentication request, controller and config

---
+++ src/main/java/ca/papercrane/api/security/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package ca.papercrane.api.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    private String password;
+
+}
