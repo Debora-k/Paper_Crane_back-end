76a6afe79ce251f3223f21a4001e05b2ad3148e5
rchesh <20080666+rchesh@users.noreply.github.com>
2023-04-06 00:34:42 -0600

    chore: add lombok annotations to task class.

--- src/main/java/ca/papercrane/api/project/task/Task.java
+++ src/main/java/ca/papercrane/api/project/task/Task.java
@@ -1,9 +1,15 @@
 package ca.papercrane.api.project.task;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "task")
 public final class Task {
@@ -31,15 +37,10 @@ public final class Task {
     @Column(name = "progress_in_work_hours", columnDefinition = "double precision default 0")
     private Double progressInWorkHours;
 
-
     //TODO: These variables below do not exist in the database yet, but should probably be added.
     private String taskName;
     private Date dateCompleted;
     private boolean isComplete;
-
-    public Task() {
-
-    }
 
     public Task(Integer projectId, String description, Date startDate, Date deadline, Double expectedWorkHours) {
         this.projectId = projectId;
@@ -47,82 +48,6 @@ public final class Task {
         this.startDate = startDate;
         this.deadline = deadline;
         this.expectedWorkHours = expectedWorkHours;
-    }
-
-    public Integer getTaskId() {
-        return taskId;
-    }
-
-    public Integer getProjectId() {
-        return projectId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String newDescription) {
-        this.description = newDescription;
-    }
-
-    public Date getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(Date newDeadline) {
-        this.deadline = newDeadline;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date newStartDate) {
-        startDate = newStartDate;
-    }
-
-    public Double getExpectedWorkHours() {
-        return expectedWorkHours;
-    }
-
-    public void setExpectedWorkHours(Double newExpectedWorkHours) {
-        expectedWorkHours = newExpectedWorkHours;
-    }
-
-    public Double getProgressInWorkHours() {
-        return progressInWorkHours;
-    }
-
-    public void setProgressInWorkHours(Double newProgressHours) {
-        progressInWorkHours = newProgressHours;
-    }
-
-    public void addProgressHours(Double amount) {
-        progressInWorkHours += amount;
-    }
-
-    public void setName(String newName) {
-        this.taskName = newName;
-    }
-
-    public boolean isComplete() {
-        return isComplete;
-    }
-
-    public void setCompleted() {
-        this.isComplete = true;
-    }
-
-    public void setUncompleted() {
-        this.isComplete = false;
-    }
-
-    public Date getCompletedDate() {
-        return dateCompleted;
-    }
-
-    public void setCompletedDate(Date newDateCompleted) {
-        this.dateCompleted = newDateCompleted;
     }
 
 }
