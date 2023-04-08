677740c7692d82ef52717d8b259fe3c1af488f3f
rchesh <20080666+rchesh@users.noreply.github.com>
2023-04-07 22:51:38 -0600

    chore: edit auth controller mappings

--- src/main/java/ca/papercrane/api/security/auth/AuthenticationController.java
+++ src/main/java/ca/papercrane/api/security/auth/AuthenticationController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")//TODO: Make not *
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/login")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
