802d3549419ad26253d45f395f0861553ce6e53a
rchesh <reececheshire@outlook.com>
2023-04-13 19:46:28 -0600

    feat: create, update, delete TaskController mappings

--- src/main/java/ca/papercrane/api/controller/TaskController.java
+++ src/main/java/ca/papercrane/api/controller/TaskController.java
@@ -36,6 +36,60 @@ public class TaskController {
     }
 
     /**
+     * Creates a new Task.
+     *
+     * @param task The new task being created.
+     * @return The tasks generated task id.
+     */
+    @PostMapping("/create")
+    public ResponseEntity<Integer> createTask(@RequestBody Task task) {
+        try {
+            val createdRequestId = taskService.addNewTask(task);
+            return new ResponseEntity<>(createdRequestId, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Updates an existing Task.
+     *
+     * @param taskId The id of the existing task.
+     * @param task   The new task details.
+     * @return The response of the request.
+     */
+    @PutMapping("/update/{requestId}")
+    public ResponseEntity<HttpStatus> updateTask(@PathVariable Integer taskId, @RequestBody Task task) {
+        try {
+            taskService.update(taskId, task);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Deletes a Task by its corresponding taskId.
+     *
+     * @param taskId The id of the task being deleted.
+     * @return The response status.
+     */
+    @DeleteMapping("/delete/{requestId}")
+    public ResponseEntity<HttpStatus> deleteTask(@PathVariable Integer taskId) {
+        try {
+            taskService.deleteByTaskId(taskId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    /**
      * Gets a task by it's id value.
      *
      * @param taskId The task id to search for.
